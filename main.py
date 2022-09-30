5ea5e0ac2590828e7d07e33f32950c31d286f8ad
GabrieleSak <gabriele.sakalauskiene@yahoo.com>
2022-09-30 14:00:55 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,26 @@
+from menu import Menu, MenuItem
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
+
+menu = Menu()
+coffee_maker = CoffeeMaker()
+money_machine = MoneyMachine()
+
+turn_off = False
+
+while not turn_off:
+
+    choice = input(f"What would you like? ({menu.get_items()}): ").lower()
+
+    if choice == "report":
+        coffee_maker.report()
+        money_machine.report()
+    elif choice == "off":
+        turn_off = True
+    elif menu.find_drink(choice) is None:
+        break
+    else:
+        drink_choice = menu.find_drink(choice)
+        if coffee_maker.is_resource_sufficient(drink_choice):
+            money_machine.make_payment(drink_choice.cost)
+            coffee_maker.make_coffee(drink_choice)
